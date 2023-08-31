7a2a622a759f33be2b759117227fb52312c9cb36
Jacob Kim <jkjacobkim@gmail.com.here>
2023-08-31 14:28:12 -0700

    Finished get property detail unit test.

--- api/tests/test_properties.py
+++ api/tests/test_properties.py
@@ -4,6 +4,7 @@ from queries.properties import PropertiesQueries
 
 client = TestClient(app)
 
+
 class FakePropertiesQuery():
     def get_properties(self):
         return [
@@ -22,6 +23,28 @@ class FakePropertiesQuery():
                 "user_id": 0
             }
             ]
+    def get_property(self, property_id: int):
+        return {
+            "id": property_id,
+            "price": 0,
+            "city": "string",
+            "bedrooms": 0,
+            "bathrooms": 0,
+            "address": "string",
+            "sq_footage": 0,
+            "year_built": 0,
+            "multistory": True,
+            "new_build": True,
+            "state": "string",
+            "owner": {
+                "id": 0,
+                "email": "string",
+                "first_name": "string",
+                "last_name": "string",
+                "phone_number": "string"
+            }
+        }
+
 
 def test_get_properties():
     app.dependency_overrides[PropertiesQueries] = FakePropertiesQuery
@@ -45,3 +68,31 @@ def test_get_properties():
                 "user_id": 0
             }
             ]
+
+
+def test_get_property_detail():
+    app.dependency_overrides[PropertiesQueries] = FakePropertiesQuery
+    res = client.get("/api/properties/1")
+    data = res.json()
+
+    assert res.status_code == 200
+    assert data == {
+        "id": 1,
+        "price": 0,
+        "city": "string",
+        "bedrooms": 0,
+        "bathrooms": 0,
+        "address": "string",
+        "sq_footage": 0,
+        "year_built": 0,
+        "multistory": True,
+        "new_build": True,
+        "state": "string",
+        "owner": {
+            "id": 0,
+            "email": "string",
+            "first_name": "string",
+            "last_name": "string",
+            "phone_number": "string"
+        }
+    }
